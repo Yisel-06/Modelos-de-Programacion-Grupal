133e9ab23e5172ad38f5b6d5934ade3f77796f12
lauVel17 <lvelandia47@estudiantes.areandina.edu.co>
2025-03-14 22:35:39 -0500

    generar-numCli

--- Eje3/cliente.py
+++ Eje3/cliente.py
@@ -1,7 +1,11 @@
 import socket  # Importar librería
-
+import random
+import threading
+import time
 host = "127.0.0.1"
 port = 8000
+
+ejecutando = True
 
 # Crear el socket
 cliente_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -9,6 +13,24 @@ cliente_socket.connect((host, port))  # Conectar al servidor
 
 print("Hola desde el cliente")
 
+
+def generar_numAl():
+    global ejecutando 
+    while ejecutando:
+        numero = random.randrange(1, 10)  
+        print(f"Cliente generó: {numero}")
+        cliente_socket.sendall(str(numero).encode())
+        time.sleep(90) #genera cada x seg
+
+hilo = threading.Thread(target=generar_numAl)
+hilo.start()
+
 while True:
-    mensaje = input("Ingrese un valor: ")
-    cliente_socket.sendall(mensaje.encode())
+    mensaje = input("Escibe fin si desea terminar el juego ")
+    if mensaje.lower() == 'fin':
+        ejecutando= False
+        cliente_socket.sendall(mensaje.encode())
+
+        break
+
+cliente_socket.close()
--- Eje3/servidor.py
+++ Eje3/servidor.py
@@ -3,14 +3,29 @@ host = "127.0.0.1" #Deben correr en el mismo host y puerto
 port =8000 #Este puerto corresponde al servidor el del cliente se asigna dinamico
 
 
+#AF_INET Tipo de direcciones a los que se conecta el servidor
+#SOCK_STREAM Tipo de socket- stream: intercambio y flujo de datos
+
 # Crear el socket
 mi_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 mi_socket.bind((host, port))
-mi_socket.listen(5)
+mi_socket.listen()
 
 print("Servidor escucha en {}:{}".format(host, port))
 
 cliente_socket, addr = mi_socket.accept()
 print("Conexionestablecida desde",addr)
+
+while True:
+     mensaje = cliente_socket.recv(1024).decode()
+     print(f"recibe: {mensaje}")
+     if mensaje.lower() == "fin":
+         break
+     
+cliente_socket.close()
+mi_socket.close()
+     
+     
+
 
 
