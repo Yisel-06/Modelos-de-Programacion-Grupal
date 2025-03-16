b7e01b5bdad0b90e4fb38a56df41466ce3bba268
Yisel-06 <yhernandez164@estudiantes.areandina.edu.co>
2025-03-16 01:48:38 -0500

    evitar_numeros_repetidos

--- Eje3/cliente.py
+++ Eje3/cliente.py
@@ -5,26 +5,37 @@ host = "127.0.0.1"
 port = 8000
 
 ejecutando = True
+numeros_cliente = []
 
 # Crear el socket
 cliente_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 cliente_socket.connect((host, port))  # Conectar al servidor
 
-print('____________________\n')
+print('__________________________________\n')
+print('         Bienvenido al juego adivina adivinador :D          ')
+print('__________________________________')
 
-print('         Bienvenido al juego adivina adivinador           ')
-print('____________________')
 
 while True:
     
+    if len(numeros_cliente) == 10:
+        numeros_cliente = []
+
     mensaje = input("Presiona 'enter' si deseas iniciar el juego o 'terminar' si deseas terminar: ")
     
     if mensaje.lower() != 'terminar':
         numero = random.randint(1, 10)
-        print(f"Cliente generó: {numero}")
+
+        # Verifica que el número generado no se repita con números anteriormente generados
+        if len(numeros_cliente) > 0:
+            while numero in numeros_cliente:
+                numero = random.randint(1, 10)
+
+        print(f"- El cliente eligió el número: {numero}")
+        print('__________________________________')
+        numeros_cliente.append(numero)
         
         cliente_socket.sendall(str(numero).encode())
-    
     
     else:
         cliente_socket.sendall(mensaje.encode())
--- Eje3/servidor.py
+++ Eje3/servidor.py
@@ -13,52 +13,68 @@ print(f"Servidor escucha en {host}:{port}")
 
 cliente_socket, addr = mi_socket.accept()
 print(f"Conexión establecida desde {addr}")
+print('__________________________________')
 
 def generar_numero_servidor():
     return random.randint(1, 10) 
 
-#Instancia de variables para contabilizar aciertos y desaciertos
+# Instancia de variables para contabilizar aciertos y desaciertos y la lista de los numeros que se han ido generando
 victorias = 0
 derrotas = 0
 derrotas_consecutivas = 0
+numeros_servidor = []
 
 while True:
     mensaje = cliente_socket.recv(1024).decode()
     
+    if len(numeros_servidor) == 10:
+        numeros_servidor = []
+        print("->    ¡El juego se reinició, buena suerte! :D    <-")
+        print('__________________________________')
+
     if mensaje.lower() == "terminar":
-        print("El cliente ha terminado el juego.")
+        print("El cliente ha terminado el juego :v")
         break
     
     else:
         numerocliente = int(mensaje)
         
         numeroservidor = generar_numero_servidor()
+
+        # Verifica que el número generado no se repita con números anteriormente generados
+        if len(numeros_servidor) > 0:
+            while numeroservidor in numeros_servidor:
+                numeroservidor = generar_numero_servidor()
         
-        print(f"Cliente envió: {numerocliente}")
-        print(f"Servidor generó: {numeroservidor}")
+        print(f"- El servidor eligió el número: {numeroservidor}")
+        numeros_servidor.append(numeroservidor)
     
         if numerocliente == numeroservidor:
-            print(f"¡Coincidencia! Ambos eligieron: {numerocliente}")
+            print(f"¡Coincidencia :D! Ambos eligieron el número '{numerocliente}'")
+            print('__________________________________')
+
             victorias += 1
             derrotas_consecutivas = 0    
             
         else:
-            print(f"No coinciden. Servidor: {numeroservidor}, Cliente: {numerocliente}")
+            print(f"Los números no coinciden :c")
+            print(f"El cliente eligió el número '{numerocliente}' y el servidor eligió el número '{numeroservidor}'")
+            print('__________________________________')
             derrotas += 1
             derrotas_consecutivas += 1
-            
-        if derrotas_consecutivas >= 3:
-            print('Tienes 3 derrotas consecutivas perdiste xd')
-            mi_socket.sendall(derrotas_consecutivas.encode())
-            break
+        
+        # Si el servidor detecta que hay 3 derrotas consecutivas, termina el juego
+        if derrotas_consecutivas == 3:
+            print('->    Tienes 3 derrotas consecutivas. Perdiste :c    <-')
+            print('__________________________________')
+            derrotas_consecutivas = 0
 
 cliente_socket.close()
 mi_socket.close()
-print('Cantidad de aciertos durante la partida: ', victorias)
-print('Cantidad de desaciertos durante la partida: ', derrotas)
+
+print('Resultados:')
+print('- Cantidad de aciertos durante la partida:', victorias)
+print('- Cantidad de desaciertos durante la partida:', derrotas)
 
 print("Conexión cerrada.")
      
-
-
-
