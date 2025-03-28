3a80a6e1a3f0458821f5bdb7f60038b493c0d128
CrisAle09 <cpinzon44@estudiantes.areandina.edu.co>
2025-03-27 23:20:44 -0500

    Implementación de Arbol

--- Eje4/servidor.py
+++ Eje4/servidor.py
@@ -1,5 +1,43 @@
 import socket
 import time
+from binarytree import Node
+
+# Definición de las clases Nodo y Árbol
+class Nodo:
+    def __init__(self, valor):
+        self.valor = valor
+        self.izquierda = None  
+        self.derecha = None
+
+class Arbol:
+    def __init__(self):
+        self.raiz = None
+
+    def insert(self, valor):
+        if self.raiz is None:
+            self.raiz = Nodo(valor)
+        else:
+            self.insert_recursive(self.raiz, valor)
+
+    def insert_recursive(self, nodo, valor):
+        if valor < nodo.valor:               
+            if nodo.izquierda is None:
+                nodo.izquierda = Nodo(valor)
+            else:
+                self.insert_recursive(nodo.izquierda, valor)
+        elif valor > nodo.valor:
+            if nodo.derecha is None:
+                nodo.derecha = Nodo(valor)
+            else:
+                self.insert_recursive(nodo.derecha, valor)
+
+    def tree(self, nodo):
+        if nodo is None:
+            return None
+        bt_nodo = Node(nodo.valor)
+        bt_nodo.left = self.tree(nodo.izquierda)
+        bt_nodo.right = self.tree(nodo.derecha)
+        return bt_nodo
 
 servidor_socket = socket.socket()
 servidor_socket.bind(("localhost", 8000))
@@ -8,6 +46,7 @@ servidor_socket.listen()
 conexion, direccion = servidor_socket.accept()
 print("Conexión establecida con:", direccion)
 
+arbol = Arbol()
 lista = []
 
 while True:
@@ -20,8 +59,13 @@ while True:
 
     if 10 <= numero_cliente < 100:
         lista.append(numero_cliente)
+        arbol.insert(numero_cliente)
         respuesta = "Número recibido y válido"
         print(f"El cliente envió: {numero_cliente}")
+        # Mostrar el árbol en consola
+        bt_raiz = arbol.tree(arbol.raiz)
+        print("\nÁrbol binario:")
+        print(bt_raiz)
     else:
         respuesta = "El número no tiene dos dígitos"
         print(f"Número incorrecto: {numero_cliente}")
@@ -29,12 +73,12 @@ while True:
     # Enviar respuesta al cliente
     conexion.sendall(respuesta.encode())
 
-    if len(lista) == 30:
+    if len(lista) == 5:
         mensaje_cierre = "Se han recibido 30 números, la conexión se cerrará."
         conexion.sendall(mensaje_cierre.encode())  # Avisar antes de cerrar
         print("La conexión se ha cerrado")
 
-        time.sleep(2)  # intervalo de tiempo para cerrar la conexión
+        time.sleep(10)  # intervalo de tiempo para cerrar la conexión
         break 
         
 print("Desconectado del cliente", direccion)
