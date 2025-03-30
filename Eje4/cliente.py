411311ea7bd636b6b93be8f91430edf9b1d43ff1
Yisel-06 <yhernandez164@estudiantes.areandina.edu.co>
2025-03-29 19:11:06 -0500

    update

--- Eje4/cliente.py
+++ Eje4/cliente.py
@@ -4,7 +4,7 @@ cliente_socket = socket.socket()  # Crea el socket
 cliente_socket.connect(("localhost", 8000))  # Conecta al servidor
 
 while True:
-    numero = input("Ingrese el número o 'Salir' para cerrar las conexiones ")
+    numero = input("Ingrese el número o 'Salir' para cerrar las conexiones: ")
 
     if numero.lower() == "salir":  # Permitir salida manual
         break
@@ -16,7 +16,7 @@ while True:
         int(numero)
     
     except ValueError:
-        print("Por favor, ingrese un número válido o 'Salir'.") 
+        print("Por favor, ingrese un número válido o 'Salir': ") 
         continue
 
     cliente_socket.sendall(str(numero).encode())  # Enviar número al servidor
--- Eje4/servidor.py
+++ Eje4/servidor.py
@@ -58,29 +58,31 @@ while True:
     numero_cliente = int(data.decode())
     if numero_cliente==numero_cliente:
         print("")
+        
     if 10 <= numero_cliente < 100:
         lista.append(numero_cliente)
         arbol.insert(numero_cliente)
+
         respuesta = "Número recibido y válido"
-        print(f"El cliente envió: {numero_cliente}")
-        # Mostrar el árbol en consola
+    
         bt_raiz = arbol.tree(arbol.raiz)
-        print("\nÁrbol binario:")
+        print("Árbol binario:")
         print(bt_raiz)
+        
+        if len(lista) == 5:
+            mensaje_cierre = "Se han recibido 30 números, la conexión se cerrará."
+            conexion.sendall(mensaje_cierre.encode())  # Avisar antes de cerrar
+            print("La conexión se ha cerrado")
+
+            time.sleep(2)  # intervalo de tiempo para cerrar la conexión
+            break 
+
     else:
         respuesta = "El número no tiene dos dígitos"
         print(f"Número incorrecto: {numero_cliente}")
-
+    
     # Enviar respuesta al cliente
     conexion.sendall(respuesta.encode())
-
-    if len(lista) == 5:
-        mensaje_cierre = "Se han recibido 30 números, la conexión se cerrará."
-        conexion.sendall(mensaje_cierre.encode())  # Avisar antes de cerrar
-        print("La conexión se ha cerrado")
-
-        time.sleep(10)  # intervalo de tiempo para cerrar la conexión
-        break 
         
 print("Desconectado del cliente", direccion)
 servidor_socket.close()
